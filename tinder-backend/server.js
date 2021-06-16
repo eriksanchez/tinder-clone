bc8570e08678ab63de51418e7724d5c594bfd4da
eriksanchez <erikcsanchezdev@gmail.com>
2021-06-15 21:54:07 -0500

    First commit

---
+++ tinder-backend/server.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cards from "./dbCards.js"
+import Cors from "cors"
+//App config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = `mongodb+srv://admin:Z5OTxJQpXnOhaBbW@cluster0.rw3pa.mongodb.net/tinderdb?retryWrites=true&w=majority`
+//Middlewares
+app.use(express.json())
+app.use(Cors());
+
+//DB Config
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+//API endpoints 
+app.get('/', (req,res) => res.status(200).send('Hello Erik'));
+app.post('/tinder/cards',(req,res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard,(err,data) =>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req,res)=>{
+    Cards.find( (err,data) =>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+//Listener
+
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
